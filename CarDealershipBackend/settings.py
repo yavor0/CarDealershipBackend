4c5c5e60c97e0aa6c85b2fdc9c9a8b3a473fe90a
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2022-05-08 18:41:46 +0300

    add cors headers to the api

--- CarDealershipBackend/settings.py
+++ CarDealershipBackend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'car',
     'debug_toolbar'
@@ -53,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'corsheaders.middleware.CorsMiddleware', # CORS middleware
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 INTERNAL_IPS = [
