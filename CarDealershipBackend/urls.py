f21e2ddbbe186f8a6c7a331695a46da85698dfec
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2022-05-08 21:53:42 +0300

    add media root + fixes

--- CarDealershipBackend/urls.py
+++ CarDealershipBackend/urls.py
@@ -17,10 +17,14 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', include('car.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
