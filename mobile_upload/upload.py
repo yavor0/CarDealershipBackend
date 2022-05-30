ffa353d5ff74d7ec83befe1980924c4f904d7f5b
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2022-05-30 18:37:33 +0300

    add fill horser power, engine type

--- mobile_upload/upload.py
+++ mobile_upload/upload.py
@@ -31,14 +31,28 @@ class MobileUploader():
     def fill_car_model(self, car_model):
         all_car_models_element = self.driver.find_element(by=By.XPATH, value='//*[@class="sw145new"][@name="f6"]')
         all_car_models = all_car_models_element.text.split("\n")
-        dropdown_with_car_makes = Select(all_car_models_element)
+        dropdown_with_car_models = Select(all_car_models_element)
         closest_car_model = process.extractOne(car_model, all_car_models)
-        dropdown_with_car_makes.select_by_value(closest_car_model[0])
+        dropdown_with_car_models.select_by_value(closest_car_model[0])
+
+    def fill_engine_type(self, engine_type):
+        all_engine_types = self.driver.find_element(by=By.XPATH, value='//*[@class="sw145new"][@name="f8"]')
+        dropdown_with_engine_types = Select(all_engine_types)
+        dropdown_with_engine_types.select_by_value(engine_type)
+
+    def fill_horse_power(self, horse_power):
+        self.driver.find_element(by=By.XPATH, value='//*[@class="sw145new"][@name="f9"]').send_keys(horse_power)
+
+    
 
     def run(self, car_make, car_model):
         self.driver.get("https://www.mobile.bg/pcgi/mobile.cgi?pubtype=1&act=1")
         self.fill_car_make(my_car_make)
         self.fill_car_model(my_car_model)
+        self.fill_engine_type('Бензинов') # this will be fixed later
+        self.fill_horse_power('123') # this will be fixed later
+
+
 
 my_car_make = "Alfa Romeo"
 my_car_model = "164"
