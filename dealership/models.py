e5f5d96799b782a779164a892ca4ab974bcc6540
yavor2 <yavor.s.pachedzhiev.2018@elsys-bg.org>
2022-06-21 00:56:56 +0300

    add image to car evaluation model

--- dealership/models.py
+++ dealership/models.py
@@ -72,5 +72,7 @@ class CarEvaluation(CarModel):
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=12)
     date_submitted = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='dealership/images')
+
     class Meta:
         ordering = ["-date_submitted"]
